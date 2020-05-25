a8933a8cd34ebcf3e48bce841112b36993eff472
Vinicius Cruvinel <31232650+vlcruvi@users.noreply.github.com>
2020-05-25 15:27:45 -0400

    Add files via upload

---
+++ vinicius.cruvinel-04-02-2020-my_turtle.py
@@ -0,0 +1,25 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("Black")
+screen.title("LaGCC CyberBridge")
+
+#MY TURTLE ENVIRONMENT
+my_turtle = turtle.Turtle()
+my_turtle.pencolor("White")
+my_turtle.pensize(1)
+my_turtle.shape("turtle")
+my_turtle.speed(5)
+
+#MOTION
+my_turtle.pensize(5)
+my_turtle.shape("square")
+my_turtle.speed(15)
+
+#my_turtle.left(45)
+#my_turtle.backward(100)
+#my_turtle.pendown()  
+#my_turtle.penup()
+
+# LOOP SECTION FOR IDE'S THAT END THE SESSIO
+my_turtle.mainloop()
